6c7a4bdfefc8dab91103291ae31cbf1815a7d818
Suriya S <suriyasingh001@gmail.com>
2025-01-21 10:55:58 +0530

    fixing build failed

--- pages/login.js
+++ pages/login.js
@@ -1,22 +1,16 @@
 
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]";
+
 
 export default async function Home() {
-	const session = await getServerSession(authOptions);
+	
 
-	if (!session) {
-		return (
-			<div className="flex justify-center mt-10">
-				Not logged in to see this
-			</div>
-		);
-	}
+	
 
 
 	return (
 		<div className="flex flex-col items-center mt-10">
-			{JSON.stringify(session, null, 2)}
+			<h1 className="text-2xl font-bold mb-4">Login</h1>
 		</div>
 	);
 }
